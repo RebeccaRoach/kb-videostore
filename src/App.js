8e6493dc740cbd7fff712485ef43d229d91d6943
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-22 16:50:40 -0700

    Created Context for session to identify state of selected customer and movie, and updating both variables with setState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -12,20 +12,32 @@ import Customers from './components/Customers.js'
 import Library from './components/Library.js'
 import Search from './components/Search.js'
 
+// we can consume the context anywhere if we export it like so:
+export const SessionContext = React.createContext();
 
 function App() {
+
+  const [selectedCustomer, setSelectedCustomer] = useState(undefined);
+  const [selectedMovie, setSelectedMovie] = useState(undefined);
+
   return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/library' component={Library} />
-          <Route path='/search' component={Search} />
-          <Route path='/customers' component={Customers} />
-        </Switch>
-      </div>
-    </Router>
+    <SessionContext.Provider value={{
+      selectedCustomer,
+      selectedMovie,
+      setSelectedCustomer,
+      setSelectedMovie}}>
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/library' component={Library} />
+            <Route path='/search' component={Search} />
+            <Route path='/customers' component={Customers} />
+          </Switch>
+        </div>
+      </Router>
+    </SessionContext.Provider>
   );
 }
 
