fbb444ab8a7c9f574b622515161a00a7d30d584e
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-25 12:43:24 -0700

    Added Search styles to Library page for listing all videos in library.

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext }  from 'react';
 import axios from 'axios';
 import { SessionContext } from "../App";
 
-function Library() {
+const Library = () => {
 
   const [library, setLibrary] = useState([]);
   const sessionContext = useContext(SessionContext);
@@ -18,21 +18,26 @@ function Library() {
   },[])
 
   return (
-    <div className="App">
-      <p>Here is the library!</p>
-      <ul>
-        {library.map((movie) => {
-          return (
-            <li>
-              <span>
-               {movie.title}
-            </span>
-            <button onClick={()=> sessionContext.setSelectedMovie(movie)}>Select</button>
-            </li>
-          )
-        }
+    <div>
+      <h3 className="library-title">KB Videos Library</h3>
+      <div className="movie-cards-container">
+        {library.map(movie =>
+          <div>
+            <div class="card promoting-card card-style">
+              <div class="card-body d-flex flex-row">
+                <div>
+                  <div class="view overlay">
+                    <img src={movie.image_url} class="movie-img" alt="movie cover image"></img>
+                  </div>
+                  <h4 class="card-title font-weight-bold mb-2">{movie.title}</h4>
+                  <p class="card-text">Release: {movie.release_date}</p>
+                </div>
+              </div>
+              <button className="add-btn" onClick={()=> sessionContext.setSelectedMovie(movie)}>Select</button>
+            </div>
+          </div>
         )}
-      </ul>
+      </div>
     </div>
   );
 }
