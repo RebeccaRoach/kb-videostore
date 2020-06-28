2111be14d140e1b9efcd15b24c486bf84579b1f4
RebeccaRoach <rebeccaroach12@gmail.com>
2020-06-28 16:16:59 -0700

    Updates

---
+++ precache-manifest.8fd92fe9e1f8780e6a29493dd15fb882.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea69fc416647fd6f6073dcd7c08ded4a",
+    "url": "/kb-videostore/index.html"
+  },
+  {
+    "revision": "ca752728cb3c0f2a76df",
+    "url": "/kb-videostore/static/css/2.0f17492b.chunk.css"
+  },
+  {
+    "revision": "d2f5f806dbcde18244b0",
+    "url": "/kb-videostore/static/css/main.10818243.chunk.css"
+  },
+  {
+    "revision": "ca752728cb3c0f2a76df",
+    "url": "/kb-videostore/static/js/2.2f1f6c3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kb-videostore/static/js/2.2f1f6c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2f5f806dbcde18244b0",
+    "url": "/kb-videostore/static/js/main.5781f6cd.chunk.js"
+  },
+  {
+    "revision": "8fafd775f524751ce626",
+    "url": "/kb-videostore/static/js/runtime-main.dd16c5cd.js"
+  }
+]);
